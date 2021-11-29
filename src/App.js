eae25a2b42a4799d29e23d14edd7677bb08b8ca1
Charles Paz <pazcharles02@gmail.com>
2021-11-29 10:49:37 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          react app has been INITIALIZZZZZEEEEDDD lesgo
         </a>
       </header>
     </div>
