e47394e15b614cb219c4cd1342dc1e50b1efe8e1
Charles Paz <pazcharles02@gmail.com>
2021-11-29 15:46:46 -0800

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-
-import React from "react";
-import Navbar from "./components/Navbar";
-import About from "./components/About";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
-
 
 function App() {
   return (
-    <main className="App">
-      <Navbar />
-      <About />
-      <Projects />
-      <Skills />
-    </main>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
