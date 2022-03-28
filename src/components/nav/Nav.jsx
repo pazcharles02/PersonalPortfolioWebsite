a6e39a4beee1f70f678895f2ba5315fe3b0198d5
pazcharles02 <pazcharles02@gmail.com>
2022-03-28 09:50:33 -0700

    navbar complete

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./nav.css"
-import {AiOutlineHome} from "react-icons/all";
+import {AiOutlineHome, BiBook, AiOutlineUser, RiServiceLine, BiMessageAltDetail} from "react-icons/all";
 
 function Nav() {
     return (
         <nav>
             <a href="#"><AiOutlineHome/></a>
+            <a href="#about"><AiOutlineUser/></a>
+            <a href="#experience"><BiBook/></a>
+            <a href="#services"><RiServiceLine/></a>
+            <a href="#contact"><BiMessageAltDetail/></a>
         </nav>
     )
 }
