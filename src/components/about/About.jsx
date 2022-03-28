b576fb1b7f7305467913d9745ceaa4e5bd4e9883
pazcharles02 <pazcharles02@gmail.com>
2022-03-28 10:30:57 -0700

    about page complete

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 import "./about.css"
+import ME from "../../assets/pfp.jpg"
+import {FaAward, VscFolderLibrary} from "react-icons/all";
 
 function About() {
     return (
         <section id="about">
-            About
+            <h5>Get To Know</h5>
+            <h2>About Me</h2>
+
+            <div className="container about__container">
+                <div className="about__me">
+                    <div className="about__me-image">
+                        <img src={ME} alt={"About Image"}/>
+                    </div>
+                </div>
+
+                <div className="about__content">
+                    <div className="about__cards">
+                        <article className="about__card">
+                            <FaAward className="about__icon"/>
+                            <h5>Experience</h5>
+                            <small>None! Hopefully you can be my first :D</small>
+                        </article>
+                        <article className="about__card">
+                            <VscFolderLibrary className="about__icon"/>
+                            <h5>Projects</h5>
+                            <small>5+ Completed!</small>
+                        </article>
+                    </div>
+
+                    <p>
+                        REEEEEEEEEEEE Lorem ipsum go here
+                    </p>
+
+                    <a href={"#contact"} className="btn btn-primary">Let's Talk</a>
+                </div>
+            </div>
+
+
         </section>
     )
 }
